f885d9b609d0dd4aafd78ccef5a0433b63c01a67
Pankaj Rawat <pankajr141@gmail.com>
2023-06-07 10:45:33 +0000

    Push New changes

---
+++ convert_ipynb_and_push.sh
@@ -0,0 +1,17 @@
+#git clone https://github.com/pankajr141/experiments.git
+
+# Removing .git from subrepo
+rm -rf experiments/.git
+
+# Convert ipynb to html
+JUPYTER_CMD=/home/pankajr141/.local/bin/jupyter
+# find ./ -type f -name *.ipynb -exec $JUPYTER_CMD nbconvert --to html {} \;
+
+
+git config --global user.email "pankajr141@gmail.com"
+git config --global user.name "Pankaj Rawat"
+
+
+git add .
+git commit -m "Push New changes"
+git push
