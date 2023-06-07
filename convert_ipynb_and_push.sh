a7fa1ec41c6d3256b6af3805932f6131b890f29b
Pankaj Rawat <pankajr141@gmail.com>
2023-06-07 11:08:52 +0000

    Push New changes

--- convert_ipynb_and_push.sh
+++ convert_ipynb_and_push.sh
@@ -1,17 +1,16 @@
-#git clone https://github.com/pankajr141/experiments.git
+pip install jupyter nbconvert
+git clone https://github.com/pankajr141/experiments.git
 
 # Removing .git from subrepo
 rm -rf experiments/.git
 
 # Convert ipynb to html
 JUPYTER_CMD=/home/pankajr141/.local/bin/jupyter
-# find ./ -type f -name *.ipynb -exec $JUPYTER_CMD nbconvert --to html {} \;
+find ./ -type f -name *.ipynb -exec $JUPYTER_CMD nbconvert --to html {} \;
 
-
+# Pushing code into GIT
 git config --global user.email "pankajr141@gmail.com"
 git config --global user.name "Pankaj Rawat"
-
-
 git add .
 git commit -m "Push New changes"
 git push
